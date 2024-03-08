28c96480056700cb0339ecaa21be22a73be333f8
andrew <andyhaz1970@gmail.com>
2024-03-08 11:54:25 -0600

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by andyhaz on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
